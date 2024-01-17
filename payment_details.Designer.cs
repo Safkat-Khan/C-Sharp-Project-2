75e4b142ab4210a86ae1b81c7ee8fb608a548853
Safkat khan Tafsir <safkatkhan420@gmail.com>
2024-01-17 22:59:38 +0600

    Payment Added

---
+++ payment_details.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace C__project
+{
+    partial class payment_details
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(payment_details));
+            panel5 = new Panel();
+            label4 = new Label();
+            panel1 = new Panel();
+            label2 = new Label();
+            label1 = new Label();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            label8 = new Label();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel5
+            // 
+            panel5.BackgroundImage = (Image)resources.GetObject("panel5.BackgroundImage");
+            panel5.BackgroundImageLayout = ImageLayout.Zoom;
+            panel5.Location = new Point(276, 3);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(180, 116);
+            panel5.TabIndex = 8;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(390, 138);
+            label4.Name = "label4";
+            label4.Size = new Size(0, 25);
+            label4.TabIndex = 14;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(230, 135, 60);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(textBox1);
+            panel1.ForeColor = SystemColors.ActiveCaptionText;
+            panel1.Location = new Point(25, 173);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(732, 220);
+            panel1.TabIndex = 15;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = Color.Black;
+            label2.Location = new Point(231, 105);
+            label2.Name = "label2";
+            label2.Size = new Size(30, 15);
+            label2.TabIndex = 3;
+            label2.Text = "PIN:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.Black;
+            label1.Location = new Point(231, 53);
+            label1.Name = "label1";
+            label1.Size = new Size(74, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Account No:";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(231, 123);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(231, 23);
+            textBox2.TabIndex = 1;
+            textBox2.Validated += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(231, 71);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(231, 23);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            textBox1.Validating += textBox1_Validating;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(61, 140, 149);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(609, 405);
+            button1.Name = "button1";
+            button1.Size = new Size(148, 35);
+            button1.TabIndex = 16;
+            button1.Text = "Buy Now";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += buyNowClick;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(61, 140, 149);
+            button2.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(25, 405);
+            button2.Name = "button2";
+            button2.Size = new Size(144, 35);
+            button2.TabIndex = 17;
+            button2.Text = "Back";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label8.Location = new Point(260, 142);
+            label8.Name = "label8";
+            label8.Size = new Size(124, 20);
+            label8.TabIndex = 18;
+            label8.Text = "Payment Option:";
+            // 
+            // payment_details
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label8);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(panel1);
+            Controls.Add(label4);
+            Controls.Add(panel5);
+            Name = "payment_details";
+            Text = "payment_details";
+            Load += payment_details_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel5;
+        private Label label4;
+        private Panel panel1;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label2;
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Label label8;
+    }
+}
---
+++ payment_details.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Data.SqlClient;
+
+namespace C__project
+{
+    public partial class payment_details : Form
+    {
+        Random rnd = new Random();
+        int number1;
+        int number2;
+        string pay_option;
+        string? id;
+        user_dashboard u1;
+        public string t_name;
+        public string f_station;
+        public string t_station;
+        public string datE;
+        public string ttl_seat;
+        public string ttl_price;
+        SqlConnection cn = new SqlConnection(@"Data Source=DESKTOP-JE75VAQ\SQLEXPRESS;Initial Catalog=c#_project(ticket);Integrated Security=True");
+
+        public void setTicket(string t_name, string f_station, string t_station, string datE, string ttl_seat, string ttl_price)
+        {
+
+            this.t_name = t_name;
+            this.f_station = f_station;
+            this.t_station = t_station;
+            this.datE = datE;
+            this.ttl_seat = ttl_seat;
+            this.ttl_price = ttl_price;
+        }
+        public payment_details(string pay_option)
+        {
+            Form1 f1 = new Form1();
+            id = f1.u_id;
+            number1 = rnd.Next(20);
+            for (int i = 0; i < 5; i++)
+            {
+                int temp = rnd.Next(20);
+                if (number1 != temp)
+                {
+                    number2 = temp;
+                }
+            }
+            InitializeComponent();
+            this.pay_option = pay_option;
+        }
+        public void setUser(user_dashboard s)
+        {
+            u1 = s;
+        }
+
+        private void payment_details_Load(object sender, EventArgs e)
+        {       
+            label4.Text = pay_option.ToString();
+            //  label4.Text = Font.Name.ToString();
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        int sum;
+        int temp;
+        bool accno, pin, capt = false;
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+
+
+
+
+
+        }
+        private void buyNowClick(object sender, EventArgs e)
+        {
+         
+
+              try
+                {
+                    cn.Open();
+                    string quary = "insert into ticket_info2 values('" + t_name + "','" + f_station + "','" + t_station + "','" + datE + "','" + ttl_seat + "','" + ttl_price + "')";
+                    SqlCommand cmd = new SqlCommand(quary, cn);
+                    int v = cmd.ExecuteNonQuery();
+                    cn.Close();
+                  MessageBox.Show("Purchasse Done");
+                    u1.Show();
+                    this.Hide();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex);
+                }
+
+
+
+        }
+
+        private void textBox1_Validating(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            payment f2 = new payment();
+            f2.Show();
+            this.Hide();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
