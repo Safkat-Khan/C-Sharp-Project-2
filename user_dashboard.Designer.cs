60ab947fe508d5a153460a00779e8330b0294b87
Safkat khan Tafsir <safkatkhan420@gmail.com>
2024-01-25 22:26:18 +0600

    Added

---
+++ user_dashboard.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace C__project
+{
+    partial class user_dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(user_dashboard));
+            panel1 = new Panel();
+            panel5 = new Panel();
+            label6 = new Label();
+            label8 = new Label();
+            label7 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            panel2 = new Panel();
+            label1 = new Label();
+            label2 = new Label();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            panel1.SuspendLayout();
+            panel5.SuspendLayout();
+            panel3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(230, 135, 60);
+            panel1.Controls.Add(panel5);
+            panel1.Controls.Add(label8);
+            panel1.Controls.Add(label7);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(label3);
+            panel1.Location = new Point(-8, 118);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(220, 334);
+            panel1.TabIndex = 0;
+            // 
+            // panel5
+            // 
+            panel5.BackColor = Color.FromArgb(61, 140, 149);
+            panel5.Controls.Add(label6);
+            panel5.Location = new Point(-9, 297);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(229, 39);
+            panel5.TabIndex = 6;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.ForeColor = Color.White;
+            label6.Location = new Point(20, 6);
+            label6.Name = "label6";
+            label6.Size = new Size(68, 20);
+            label6.TabIndex = 3;
+            label6.Text = "Sign Out";
+            label6.Click += label6_Click;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label8.ForeColor = Color.White;
+            label8.Location = new Point(11, 170);
+            label8.Name = "label8";
+            label8.Size = new Size(98, 20);
+            label8.TabIndex = 5;
+            label8.Text = "Avaible Train";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.ForeColor = Color.White;
+            label7.Location = new Point(11, 132);
+            label7.Name = "label7";
+            label7.Size = new Size(79, 20);
+            label7.TabIndex = 4;
+            label7.Text = "Buy Ticket";
+            label7.Click += label7_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.ForeColor = Color.White;
+            label5.Location = new Point(11, 206);
+            label5.Name = "label5";
+            label5.Size = new Size(37, 20);
+            label5.TabIndex = 2;
+            label5.Text = "Cart";
+            label5.Click += label5_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(11, 93);
+            label4.Name = "label4";
+            label4.Size = new Size(54, 20);
+            label4.TabIndex = 1;
+            label4.Text = "Profile";
+            label4.Click += label4_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(11, 57);
+            label3.Name = "label3";
+            label3.Size = new Size(50, 20);
+            label3.TabIndex = 0;
+            label3.Text = "Home";
+            // 
+            // panel2
+            // 
+            panel2.BackgroundImage = (Image)resources.GetObject("panel2.BackgroundImage");
+            panel2.BackgroundImageLayout = ImageLayout.Zoom;
+            panel2.Location = new Point(34, 8);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(103, 60);
+            panel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(26, 84);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 17);
+            label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(26, 101);
+            label2.Name = "label2";
+            label2.Size = new Size(0, 17);
+            label2.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.FromArgb(61, 140, 149);
+            panel3.Controls.Add(panel2);
+            panel3.Controls.Add(label2);
+            panel3.Controls.Add(label1);
+            panel3.Location = new Point(-8, 0);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(220, 127);
+            panel3.TabIndex = 3;
+            // 
+            // panel4
+            // 
+            panel4.BackgroundImage = (Image)resources.GetObject("panel4.BackgroundImage");
+            panel4.BackgroundImageLayout = ImageLayout.Zoom;
+            panel4.Location = new Point(240, 39);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(519, 354);
+            panel4.TabIndex = 4;
+            // 
+            // user_dashboard
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
+            Controls.Add(panel1);
+            Name = "user_dashboard";
+            Text = "user_dashboard";
+            Load += user_dashboard_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Label label2;
+        private Label label1;
+        private Panel panel3;
+        private Label label6;
+        private Label label4;
+        private Label label3;
+        private Panel panel4;
+        private Panel panel5;
+        private Label label8;
+        private Label label7;
+        private Label label5;
+    }
+}
